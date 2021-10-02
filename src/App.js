8064bdbc613473aa8a2f150c3b482dac08d3229a
Henry Nawrocki <hdev@sztanski.com>
2021-10-02 09:48:42 -0500

    starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="parent">
+        <div className="left">
+          <h1>left</h1>
+          <form action="">
+            <input type="email" />
+            <button type="submit">enviar</button>
+          </form>
+        </div>
+
+        <div className="right">
+          <h2>Flexbox - Split Screen Layout</h2>
+          <p>Example of a split screen layout that uses flexbox.</p>
+          <p>
+            A media query is used to detect if the virewport is below 600px, if
+            it is then <b>flex-direction</b> is set to column-reverse, so that
+            the image is displayed after the copy, useful on mobile devices!.
+          </p>
+          <p>Reduce the window size to below 600px to see an example.</p>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
